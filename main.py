e6d4a8aa8924c2eced34a1152ae137322ea831ac
whodatboiii <alexanderliden1@gmail.com>
2023-05-03 11:13:19 +0200

    Update main.py
    added a few constraints, still in early phase and improvements can be made (no solution is found)

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def project_data():
         "Flowmap CH 01.01.2022 - 31.10.2022.xlsx", sheet_name="locations",
         names=['id', 'name', 'lat', 'lon'])
 
-    locations = locations.sample(n=10, random_state=1)
+    locations = locations.sample(n=5, random_state=1)
     return database, locations
 
 def add_warehouse(locations, warehouse_loc):
@@ -36,7 +36,7 @@ def haversine(lat1, lon1, lat2, lon2):
     return R * c
 
 def weighted_distance_callback(manager, database, locations, from_index, to_index):
-    """Returns the weighted distance between two locations considering charging cost and popularity."""
+    """Returns the weighted distance between two locations considering charging cost, popularity, and charging time."""
     from_node = manager.IndexToNode(from_index)
     to_node = manager.IndexToNode(to_index)
     lat1, lon1 = locations.at[from_node, 'lat'], locations.at[from_node, 'lon']
@@ -46,11 +46,13 @@ def weighted_distance_callback(manager, database, locations, from_index, to_inde
     if to_node != from_node:
         popularity_weight = 1 + database.at[to_node, 'count'] / database['count'].max()
         charge_cost = 5
-        weighted_distance = distance * popularity_weight + charge_cost
+        charging_time = 0.2 * database.at[to_node, 'count']
+        weighted_distance = distance * popularity_weight + charge_cost + charging_time
     else:
         weighted_distance = distance
 
     return int(weighted_distance)
+
 
 def print_solution(manager, routing, solution, database, locations):
     """Prints the solution."""
@@ -63,11 +65,19 @@ def print_solution(manager, routing, solution, database, locations):
             node_index = manager.IndexToNode(index)
             next_node_index = manager.IndexToNode(solution.Value(routing.NextVar(index)))
             route_distance += weighted_distance_callback(manager, database, locations, index, solution.Value(routing.NextVar(index)))
-            plan_output += f"{node_index} -> "
+            current_location = locations.iloc[node_index]
+            next_location = locations.iloc[next_node_index]
+            plan_output += f"{current_location['name']} ({current_location['id']}) -> "
             index = solution.Value(routing.NextVar(index))
-        plan_output += f"{manager.IndexToNode(index)}\n"
+        last_location = locations.iloc[manager.IndexToNode(index)]
+        plan_output += f"{last_location['name']} ({last_location['id']})\n"
         plan_output += f"Route distance: {route_distance} units\n"
         print(plan_output)
+
+
+def demand_callback(database, manager, node):
+    """Returns the demand of a node."""
+    return database.at[node, 'count']
 
 def modelling():
     database, locations = project_data()
@@ -78,20 +88,32 @@ def modelling():
     # Create the routing model
     num_nodes = len(locations)
     depot_index = num_nodes - 1
-    num_vehicles = 3
+    num_vehicles = 15
     manager = pywrapcp.RoutingIndexManager(num_nodes, num_vehicles, depot_index)
     model = pywrapcp.RoutingModel(manager)
 
     # Define the weighted distance callback
-    transit_callback_index = model.RegisterTransitCallback(lambda from_index, to_index: weighted_distance_callback(manager, locations, from_index, to_index))
+    transit_callback_index = model.RegisterTransitCallback(lambda from_index, to_index: weighted_distance_callback(manager, database, locations, from_index, to_index))
 
     # Set the cost of travel
     model.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
 
+    # Define the demand callback
+    demand_callback_index = model.RegisterUnaryTransitCallback(lambda index: demand_callback(database, manager, manager.IndexToNode(index)))
+
+    # Set the demand function
+    model.AddDimensionWithVehicleCapacity(
+        demand_callback_index,
+        0,  # null capacity slack
+        [200] * num_vehicles,  # vehicle capacity of 200 bike batteries
+        True,  # start cumul to zero
+        "Capacity"
+    )
+
     # Solve the problem
     search_parameters = pywrapcp.DefaultRoutingSearchParameters()
-    search_parameters.first_solution_strategy = (routing_enums_pb2.FirstSolutionStrategy.GLOBAL_CHEAPEST_ARC)
-    search_parameters.time_limit.seconds = 60  # Set time limit to 60 seconds
+    search_parameters.first_solution_strategy = (routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+    search_parameters.time_limit.seconds = 120  # Set time limit to 60 seconds
 
     solution = model.SolveWithParameters(search_parameters)
 
@@ -100,6 +122,7 @@ def modelling():
         print_solution(manager=manager, routing=model, solution=solution, locations=locations, database=database)
     else:
         print('No solution found.')
+
 
 if __name__ == '__main__':
     modelling()
